217a7c584aa1ecd59933283ae49a193b65f0e110
Srinivas <kolipakasrinivas456@gmail.com>
2023-01-11 09:21:05 +0530

    delated all files startd from first

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import './css/style.css'
 
 
-import Home from './pages/Home-page/Homepage.component'
-import MenuItems from './pages/MenuI-tems/MenuItems.components'
-import Restaurant from './components/Restaurant/Restaurant.component'
-
-
-
-import {Route, Routes} from 'react-router-dom'
 
 
 function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/quick-search/:meal_id' element={<MenuItems/>}/>
-        <Route path='/Restaurant/:id' element={<Restaurant/>}/>
-
-      </Routes>
+     
       
     </div>
   )
